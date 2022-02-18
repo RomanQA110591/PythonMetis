71929ee6e53e5dd1c56b49e4b88edb0edd1fcfc8
RomanQA110591 <roman.sapachev@mail.ru>
2022-02-18 16:47:27 +0300

    DZ_dorabotka

--- conftest.py
+++ conftest.py
@@ -1,23 +1,95 @@
 import pytest
-import json
-from model.proekt import Proekt
 from fixture.application import Application
+import json
+import os.path
+import importlib
+import jsonpickle
+import ftputil
 
-
+fixture = None
 target = None
 
 
-@pytest.fixture
-def app(request):
+def load_config(file):
     global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+@pytest.fixture
+def app(request, config):
+    global fixture
     browser = request.config.getoption("--browser")
-    with open(request.config.getoption("--target")) as config_fale:
-        target = json.load(config_fale)
-    fixture = Application(browser=browser, base_url=target['base_url'])
-    request.addfinalizer(fixture.destroy)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, config=config)
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+
+    request.addfinalizer(fin)
     return fixture
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
+
+
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
+    def fin():
+        restore_install_server_configuration(config['ftp']['host'], config['ftp']['username'],
+                                             config['ftp']['password'])
+
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
--- test/test_soap.py
+++
@@ -1,7 +0,0 @@
-import string
-
-
-def test_soap(app):
-    username = "administrator"
-    password = "123"
-    assert app.soap.can_login(username, password)
